6baee83bbae384a9ea646f9c68f741cadb05cdf3
Fredrik Magnusson <raekon92@gmail.com>
2023-04-27 23:50:52 +0200

    restructure

--- Discord_bot/src/tests/test_openai/test_prompt.py
+++ Discord_bot/src/tests/test_openai/test_prompt.py
@@ -2,14 +2,12 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 
-from chatgpt.openai_api import chatgpt_response
+from AI.openai_api import chatgpt_response
 
 
 @pytest.mark.asyncio
 async def test_chatgpt_response():
     prompt = "What is the capital of France?"
-
-    # Mock the OpenAI API response
     mock_response = MagicMock()
     mock_response.choices = [{"message": {"content": "The capital of France is Paris."}}]
 
@@ -22,8 +20,6 @@ async def test_chatgpt_response():
 @pytest.mark.asyncio
 async def test_chatgpt_response_empty():
     prompt = "What is the capital of France?"
-
-    # Mock an empty OpenAI API response
     mock_response = MagicMock()
     mock_response.choices = []
 
